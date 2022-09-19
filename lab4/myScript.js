47f7e06b81c1f5e895bd44538b45009c590bedce
larsjemt <erik.jemt@telia.com>
2022-09-19 15:49:03 +0200

    Lab 4 JavaScript. First cut bare bone version

---
+++ lab4/myScript.js
@@ -0,0 +1,8 @@
+var number1 = 1;
+var number2 = 2;
+const person = {forName:"Lars-Erik", lastName:"Jemt", age:57 };
+
+console.log (number1 + " plus " + number2 + " är: " + (number1+number2));
+console.log (number1 + " minus " + number2 + " är: " + (number1-number2));
+console.log (number1 + " gånger " + number2 + " är: " + (number1*number2));
+console.log ("Hej mitt namn är " + person.forName + " " + person.lastName + " och jag är " + person.age + " år gammal");
