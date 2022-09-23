1c17ab92fe12b7726939aef0e8dab307a5df23ea
larsjemt <erik.jemt@telia.com>
2022-09-23 12:18:42 +0200

    Lab 6: Arrow function och JSDoc test

--- lab4/myScript.js
+++ lab4/myScript.js
@@ -6,3 +6,26 @@ console.log (number1 + " plus " + number2 + " är: " + (number1+number2));
 console.log (number1 + " minus " + number2 + " är: " + (number1-number2));
 console.log (number1 + " gånger " + number2 + " är: " + (number1*number2));
 console.log ("Hej mitt namn är " + person.forName + " " + person.lastName + " och jag är " + person.age + " år gammal");
+
+
+
+/**********************************************/
+/*** Lite testkod som inte hör till labben  ***/
+/**********************************************/
+
+let test = "hej";
+
+function testFunction (test) {
+    test = "Hejdå";
+    console.log(test);
+}
+testFunction();
+
+console.log(test);
+
+let firstName = "John";
+let lastName = "Doe";
+let text = `Welcome ${firstName}, ${lastName}!`;
+console.log(text);
+text = "Welcome "+firstName+ ", "+ lastName + " !";
+console.log(text);
